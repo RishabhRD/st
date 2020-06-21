4814224e014c35aab6f0cda63b60e9913ed98e49
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-06-21 21:01:06 +0530

    added correct fonts

--- config.def.h
+++ config.def.h
@@ -5,13 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-/* Spare fonts */
-static char *font2[] = {
-/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
-/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
-};
-
+static char *font = "mono:pixelsize=14:antialias=true:autohint=true";
+static char *font2[] = { "Inconsolata:pixelsize=11:antialias=true:autohint=true","SymbolsNerdFont:pixelsize=13:antialias=true:autohint=true","LohitDevanagari:pixelsize=13:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
